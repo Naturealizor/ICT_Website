312115ed8ac698a32db102f5d0a9c894f9928e64
Naturealizor <31334963+Naturealizor@users.noreply.github.com>
2018-11-20 14:17:12 -0600

    Initial commit

---
+++ View/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <ul class="socialmedia">
+        <li> <a href="Social_Media/Ashley%20(@ashesthehuman)%20%E2%80%A2%20Instagram%20photos%20and%20videos.html"><u>Instagram</u></a></li>
+        <li> <a href="Social_Media/Ashley%20Vieux%20(@ashesvieux)%20_%20Twitter.html"><u>Twitter</u></a></li>
+        <li> <a href="Gallery/index.html"><u>Gallery</u></a></li>      
+</footer>
+</body>
+</html>
---
+++ View/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Ashley'sWebsite</title>
+    <link rel="stylesheet" type="text/css" href="CSS/style.css">
+</head>  
+    <body>
+        <header>
+        <h1>Welcome to IBakeICT</h1>
+        </header>
