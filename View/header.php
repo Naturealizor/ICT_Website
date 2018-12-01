fb6caa154caf4726d3e1f48dbdba166f4bfefde6
Naturealizor <31334963+Naturealizor@users.noreply.github.com>
2018-12-01 14:43:52 -0600

    building MVC

--- View/header.php
+++ View/header.php
@@ -2,10 +2,15 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Ashley'sWebsite</title>
+    <title>IBakeICT</title>
     <link rel="stylesheet" type="text/css" href="CSS/style.css">
+    
 </head>  
     <body>
+    <ul class="socialmedia">
+        <li> <a href="Social_Media/Ashley%20(@ashesthehuman)%20%E2%80%A2%20Instagram%20photos%20and%20videos.html"><u>Instagram</u></a></li>
+        <li> <a href="Social_Media/Ashley%20Vieux%20(@ashesvieux)%20_%20Twitter.html"><u>Twitter</u></a></li>
+        <li> <a href="Gallery/index.html"><u>Gallery</u></a></li>
         <header>
         <h1>Welcome to IBakeICT</h1>
         </header>
