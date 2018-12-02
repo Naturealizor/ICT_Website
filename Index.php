a5281bc908c1745bfc4da722d39d5ea7051fd38c
Naturealizor <31334963+Naturealizor@users.noreply.github.com>
2018-12-02 09:50:01 -0600

    Changed CSS, cookie file names, and moved home.php into view

--- Index.php
+++ Index.php
@@ -4,6 +4,9 @@ $lifetime = 60 * 60 * 24 * 14;    // 2 weeks in seconds
 session_set_cookie_params($lifetime, '/');
 session_start();
 
+require_once('model/config.php');
+require_once('model/user_db.php');
+
 //Getting the action function
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
@@ -17,6 +20,6 @@ if ($action == NULL) {
 
 
 
-include('home.php'); ?>
+include('view/home.php'); ?>
        
        
