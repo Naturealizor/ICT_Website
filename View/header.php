da77f0f990529155f992987bac2f092d9cd9d972
Naturealizor <31334963+Naturealizor@users.noreply.github.com>
2018-12-09 14:20:04 -0600

    cleaned up header.php

--- View/header.php
+++ View/header.php
@@ -1,5 +1,3 @@
-
-
 <html lang="en">
 <head>
     <meta charset="utf-8" />
@@ -10,25 +8,8 @@
 </head>  
     <body>
         <header>
-        <!-- Logo
-        <div id="logo">    
-        <h1>IBakeICT</h1>
-        </div> -->
-
-        <!-- Nav
-        <nav id="navi">
-        <h2>Links to each page</h2>
-        <li> <a href="index.php"><u>Home</u></a></li><br>
-        <li> <a href="gallery/gallery_home.php"><u>Gallery / Shop</u></a></li><br>
-        <li> <a href="ordering_screen/index.php"><u>Shop</u></a></li><br>
-        <li> <a href="shopping_cart/index.php"><u>Cart</u></a></li><br>
-        
-        </nav> -->
-        
         </header>
         <main> 
-            
- 
         </main>
     </body>
 </html>
